75885b73888877328af51edffb23741c7ec0146d
Paul Sessbruegger <paul.at.home@gmx.de>
2022-12-19 00:30:49 +0100

    about page started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   root to: "pages#home"
+  # get 'pages', to: 'pages#about_us'
+  get '/about', to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
