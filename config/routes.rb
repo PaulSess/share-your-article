a3202ed9e58dc53d8f252c750adf29f44e02f4bb
Paul Sessbruegger <paul.at.home@gmx.de>
2022-12-22 03:54:43 +0100

    created page and display profile image and useremail

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root to: "pages#home"
-  # get 'pages', to: 'pages#about_us'
+
+
   get '/about', to: "pages#about"
+  get '/profile', to: "pages#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
